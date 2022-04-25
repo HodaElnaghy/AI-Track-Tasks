bd52ee76033a79755f3eb7bd620cd83d9c6344d7
HodaElnaghy <96389841+HodaElnaghy@users.noreply.github.com>
2022-04-25 17:38:01 +0200

    Add files via upload

---
+++ Task2/main.py
@@ -0,0 +1,13 @@
+z=200/100  #z=2.0
+
+
+k=(1.1+2.2!=3.3 )
+# 1.1 = 1.0001100110011001101 ,, 2.2 = 10.00110011001100110011
+# so, the machine will add the two binary numbers together
+# and the value will be approximately but not exactly equal 3.3
+# so K = True
+
+
+y=  k and isinstance(z,int)
+# y = False
+#as z is not integer
