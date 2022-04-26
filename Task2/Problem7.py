ad445ef1780e80d26f53e5ba89ec32cd2e8c82d6
HodaElnaghy <96389841+HodaElnaghy@users.noreply.github.com>
2022-04-27 01:55:13 +0200

    Update Problem7.py

--- Task2/Problem7.py
+++ Task2/Problem7.py
@@ -1,25 +1,13 @@
-dict1= {
-    "name":"Doja",
-    "age":53
-}
-dict2 = {
-    "job": "Teacher"
-}
+def Rotations(arr,n,k):
+    arr_of_rotates=[0] * n
+    for i in range(n):
+        arr_of_rotates[i] = arr[i]
 
-#Method 1
-Merged1={**dict1,**dict2}
-print(Merged1)
+    for i in range(n):
+        arr_of_rotates[(i+k)%n]=arr[i]
 
-#method 2
-
-Merged2 ={key : value for dict in (dict1,dict2) for key , value in dict.items()}
-print(Merged2)
-
-#method 3
-Merged3= dict1.copy()
-Merged3.update(dict2)
-print(Merged3)
-
-#method 4
-Merged4= dict1 | dict2
-print(Merged4)
+    return (arr_of_rotates)
+arr = list(map(int, input("Enter the array: ").split()))
+n=len(arr)
+k= int(input("Enter the number of rotations: "))
+print("The array after Rotations: ",Rotations(arr,n,k))
