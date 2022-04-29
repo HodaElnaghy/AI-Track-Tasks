661f266e61617549969edc7fb645dc98314137af
HodaElnaghy <96389841+HodaElnaghy@users.noreply.github.com>
2022-04-29 18:23:49 +0200

    Update Problem2.py

--- Task3/Problem2.py
+++ Task3/Problem2.py
@@ -1,3 +1,13 @@
+def First_Last_Index(arr,n,target):
+    index1= arr.index(target)
+    arr.reverse()
+    index2= n-1-arr.index(target)
+    return (index1, index2)
+
+
+def Sorting(arr,n,target):
+    arr.sort()
+    return(First_Last_Index(arr,n,target))
 
 is_even = lambda num : num%2
 
@@ -8,5 +18,7 @@ def even_count(arr):
             count+=1
     return count
 
-arr = list(map(int, input("Enter the array: ").split()))
-print(even_count(arr))
+
+if __name__ == '__main__':
+    arr = list(map(int, input("Enter the array: ").split()))
+    print(even_count(arr))
