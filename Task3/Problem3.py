a2d8b29153121bf405f04a9642d14065b83ed195
HodaElnaghy <96389841+HodaElnaghy@users.noreply.github.com>
2022-04-29 19:13:25 +0200

    Update Problem3.py

--- Task3/Problem3.py
+++ Task3/Problem3.py
@@ -1,5 +1,21 @@
-while(1):
+from main2 import Sorting
+x=True
+y=True
+
+while(x):
     try:
-        import main2
+        arr = list(map(int, input("Enter the array: ").split()))
+        n = len(arr)
+        x=False
+
     except:
-        print("Enter integers")
+        print("Enter array of integers! ")
+
+while(y):
+    try:
+        target=int(input("Enter the target number: "))
+        print(Sorting(arr, n, target))
+        y=False
+    except:
+        print("Enter an integer target within range")
+
